32e9ee41889619a792974da4769957322f5c3bfa
Phi <34006171@parisnanterre.fr>
2022-12-02 19:31:23 +0100

    Update main class

--- main.py
+++ main.py
@@ -4,20 +4,24 @@ from settings import *
 class Game:
 
     def __init__(self):
-        pygame.init () 
+        pygame.init () # Initialization
+        # Screen
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pygame.display.set_caption('Yoga Surf Land')
         self.clock = pygame.time.Clock()
 
     def run(self):
         while True:
+            # Checking if we are closing the game
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
-                    sys.exit()
+                    sys.exit() # Close the game
 
-            dt = self.clock.tick() / 1000
+            dt = self.clock.tick() / 1000 # datatime
             pygame.display.update()
 
+# Checking if we are in the main file
 if __name__ == '__main__':
     game = Game ()
     game.run()
