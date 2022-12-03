e580054f118f1e140d4c33b4153867101678f33b
Phi <34006171@parisnanterre.fr>
2022-12-03 11:01:25 +0100

    debug AttributeError : Level object has no attribute all_sprites_draw

--- level.py
+++ level.py
@@ -2,19 +2,15 @@ import pygame
 from settings import *
 
 class Level:
-    def __init__(self):
-        # attributes
+	def __init__(self):
 
-        # get the display surface
-        self.display_surface = pygame.display.get_surface()
+		# get the display surface
+		self.display_surface = pygame.display.get_surface()
 
-        # sprite groups
-        self.all_sprites = pygame.sprite.Group()
+		# sprite groups
+		self.all_sprites = pygame.sprite.Group()
 
-    def run(self,dt):
-        self.display_surface.fill('pink')
-        self.all_sprites_draw(self.display_surface)
-        self.all_sprites.update()
-        #print('The game is currently running...')
-
-
+	def run(self,dt):
+		self.display_surface.fill('pink')
+		self.all_sprites.draw(self.display_surface)
+		self.all_sprites.update()
--- main.py
+++ main.py
@@ -1,30 +1,26 @@
-import pygame, sys 
+import pygame, sys
 from settings import *
 from level import Level
 
 class Game:
+	def __init__(self):
+		pygame.init()
+		self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+		pygame.display.set_caption('Cozy Land')
+		self.clock = pygame.time.Clock()
+		self.level = Level()
 
-    def __init__(self):
-        pygame.init () # Initialization
-        # attributes
-        self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-        pygame.display.set_caption('Yoga Surf Land')
-        self.clock = pygame.time.Clock()
-        self.level = Level()
+	def run(self):
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					sys.exit()
+  
+			dt = self.clock.tick() / 1000
+			self.level.run(dt)
+			pygame.display.update()
 
-    def run(self):
-        while True:
-            # Checking if we are closing the game
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit() # Close the game
-
-            dt = self.clock.tick() / 1000 # datatime
-            self.level.run(dt)
-            pygame.display.update()
-
-# Checking if we are in the main file
 if __name__ == '__main__':
-    game = Game ()
-    game.run()
+	game = Game()
+	game.run()
