5e9cc920af8539649a96e0ab05c12b0c5099efd1
Phi <34006171@parisnanterre.fr>
2023-01-15 15:26:09 +0100

    Update some line codes

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+""" 
+This code is a basic implementation of a game loop using the Pygame library in Python.
+The code defines a class called "Game" which has two methods : "init" and "run".
+
+The "init" method is called when an instance of the class is created. 
+It initializes the Pygame library, sets up the game screen with a size and title,
+creates a clock object to track the passage of time, and creates an instance of the "Level" class.
+
+The "run" method contains a while loop that constantly updates the game screen. Within the loop, 
+the code checks for a QUIT event (when the user closes the game window) and exits the game if one is detected. 
+The method also calculates the amount of time that has passed since the last frame update using the clock object, 
+passes this value to the "run" method of the "level" object and updates the screen.
+
+The last line of code creates an instance of the "Game" class and starts the game loop.
+
+"""
+
 import pygame, sys
 from settings import *
 from level import Level
@@ -6,7 +23,7 @@ class Game:
 	def __init__(self):
 		pygame.init()
 		self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-		pygame.display.set_caption('Sprout land')
+		pygame.display.set_caption('Cozy land')
 		self.clock = pygame.time.Clock()
 		self.level = Level()
 
@@ -21,6 +38,9 @@ class Game:
 			self.level.run(dt)
 			pygame.display.update()
 
+# Game loop
 if __name__ == '__main__':
+	# Creates an instance of the Game class
 	game = Game()
+	# Calls the run method
 	game.run()
--- player.py
+++ player.py
@@ -1,3 +1,26 @@
+"""
+This code defines a class called "Player" that is a child class of pygame.sprite.Sprite, 
+which is a Pygame module for creating sprites (moving images) in a game.
+The class has four methods: __init__, input, move, and update.
+
+The __init__ method is called when an instance of the class is created. It takes two arguments pos and group, 
+it creates an instance of the Sprite and assigns it to the group passed as a parameter. It also creates a pink 
+colored surface of size 32x64 pixels, and creates a rect object that is centered at the position passed as a 
+parameter. It also creates two attributes, speed and direction which are used for movement, and pos a Vector2 object of Pygame.
+
+The input method is used to handle the inputs from the player by reading the keyboard state, it checks if the up, down, 
+left or right keys are pressed and sets the direction attribute accordingly.
+
+The move method is used to move the sprite based on the direction and speed attributes, it updates the position of the 
+sprite by adding the product of direction, speed and the time passed since the last frame update (dt) to the current 
+position of the sprite.
+
+The update method is used to call the input method and update the sprite based on the inputs.
+
+This class is using settings module, which contain the settings for the game.
+
+"""
+
 import pygame
 from settings import *
 
@@ -7,6 +30,57 @@ class Player(pygame.sprite.Sprite):
 
 		# general setup
 		self.image = pygame.Surface((32,64))
-		self.image.fill('green')
+		self.image.fill('pink')
 		self.rect = self.image.get_rect(center = pos)
+
+		# movement attributes
+		self.speed = 200
+		self.direction = pygame.math.Vector2()
+		self.pos = pygame.math.Vector2(self.rect.center)
+
+	def input(self):
+		keys = pygame.key.get_pressed()
+
+		if keys[pygame.K_UP]:
+			self.direction.y = -1
+		elif keys[pygame.K_DOWN]:
+			self.direction.y = 1
+		else:
+			self.direction.y = 0
+
+		if keys[pygame.K_RIGHT]:
+			self.direction.x = 1
+		elif keys[pygame.K_LEFT]:
+			self.direction.x = -1
+		else:
+			self.direction.x = 0
+
+		#print(self.direction)	
+		# "dt" represents the elapsed time since the last frame update
+
+	def move(self,dt):
+
+		# normalizing a vector 
+		"""The first step in the method is to normalize the direction vector. This is done by dividing the vector by its magnitude. 
+		This ensures that the direction vector has a length of 1, which is important for ensuring that the player moves at a 
+		consistent speed."""
+		if self.direction.magnitude() > 0:
+			self.direction = self.direction.normalize()
+
+		"""The next step is to update the player's position. The x and y components of the position vector are both incremented 
+		by the corresponding components of the direction vector, multiplied by the player's speed and dt. This will move the player
+		in the direction specified by the direction vector, at a speed of "speed" units per second. 
+		Finally, the rect's centerx and centery attributes are set to the player's x and y position respectively, to reflect the
+		change in position."""
+		# horizontal movement                                                          
+		self.pos.x += self.direction.x * self.speed * dt                                                                                             
+		self.rect.centerx = self.pos.x
+		# vertical movement
+		self.pos.y += self.direction.y * self.speed * dt
+		self.rect.centery = self.pos.y
+
+	def update(self,dt):
+		self.input()
+		self.move(dt)
+
 
